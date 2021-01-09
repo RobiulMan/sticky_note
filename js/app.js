e3644e4876ffb4a0feeb56955b9259d19d6b719a
RobiulMan <robiulislam.bg@gmail.com>
2021-01-10 01:51:47 +0600

    all most done

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ let dateSetter = document.querySelector('#set-dates')
 let noteBodyText = document.querySelector('#body-text')
 let addBtn = document.querySelector('#add-btn')
 
+//get html card element
+
 
 /**
  * future i will implamentrion the color area
@@ -19,6 +21,10 @@ let addBtn = document.querySelector('#add-btn')
 loadEventListners()
 //load all event listeners
 function loadEventListners() {
+    
+    //DOM load event 
+    document.addEventListener('DOMContentLoaded', getCard)
+    
     //date function 
     dates()
     
@@ -35,8 +41,8 @@ function loadEventListners() {
     noteArea.addEventListener('click', reWriteAction)
     
 /**
-* future i will implamentrion the color area
-*/
+* @color future i will implamentrion the color area
+**/
     //color plate event 
     // colorBtn.addEventListener('click', colorvBtnPlate)
 
@@ -44,18 +50,16 @@ function loadEventListners() {
     // plates.forEach(item => item.addEventListener('click', colorSelectore))
 }
 
-//add sticky note content
-function addContent(e) {
-    //check input filed
-    if(noteTitle.value === '') {
-        alert('add something')
-        return false
+// get card from LS
+function getCard() {
+    let data;
+    if(localStorage.getItem('data') === null) {
+        data = []
+    }else{
+        data = JSON.parse(localStorage.getItem('data'))
     }
-    if(noteBodyText.value === '') {
-        alert('add something')
-        return false
-    }
-
+    data.forEach(function(item) {
+        
     //creat note element
     const rootNoteElement = elementMaker('div', 'modal-root col-md-6 col-lg-4 mb-3')
     const seconderyNoteElement = elementMaker('div', 'modals')
@@ -63,7 +67,7 @@ function addContent(e) {
     const noteHeadElement = elementMaker('div', 'modal-header')
     const noteHeadTextElemnt = elementMaker('h5', '')
     const notsBodyElement = elementMaker('div', 'modal-body')
-    const noteBodyTextElement = elementMaker('p', 'lead fix-size')
+    const noteBodyTextElement = elementMaker('p', 'fix-size')
     const notesDatesElement = elementMaker('div', 'dates mt-2 m col-8 d-flex justify-content-center')
     const notesDatesTextElement = elementMaker('small','')
 
@@ -91,6 +95,69 @@ function addContent(e) {
     // <span class="material-icons" > add_task </span>
     notesDatesTextElement.appendChild(document.createTextNode(`Last Update: ${dates()}`))
     notesDatesElement.appendChild(notesDatesTextElement)
+    noteHeadTextElemnt.appendChild(document.createTextNode(item[0]))
+    noteBodyTextElement.appendChild(document.createTextNode(item[1]))
+    noteContinerElement.appendChild(noteHeadElement)
+    noteHeadElement.appendChild(noteHeadTextElemnt)
+    noteContinerElement.appendChild(notesDatesElement)
+    noteContinerElement.appendChild(notsBodyElement)
+    notsBodyElement.appendChild(noteBodyTextElement)
+    noteContinerElement.appendChild(editeBtnElement)
+    
+    
+    seconderyNoteElement.appendChild(noteContinerElement)
+    rootNoteElement.appendChild(seconderyNoteElement)
+    noteArea.appendChild(rootNoteElement)
+
+    })
+}
+//add sticky note content
+function addContent(e) {
+    //check input filed
+    if(noteTitle.value === '') {
+        alert('add something')
+        return false
+    }
+    if(noteBodyText.value === '') {
+        alert('add something')
+        return false
+    }
+
+    //creat note element
+    const rootNoteElement = elementMaker('div', 'modal-root col-md-6 col-lg-4 mb-3')
+    const seconderyNoteElement = elementMaker('div', 'modals')
+    const noteContinerElement = elementMaker('div', 'modal-content')
+    const noteHeadElement = elementMaker('div', 'modal-header')
+    const noteHeadTextElemnt = elementMaker('h5', '')
+    const notsBodyElement = elementMaker('div', 'modal-body')
+    const noteBodyTextElement = elementMaker('p', 'fix-size')
+    const notesDatesElement = elementMaker('div', 'dates mt-2 m col-8 d-flex justify-content-center')
+    const notesDatesTextElement = elementMaker('small','')
+
+    //button area
+    const editeBtnElement = elementMaker('div', 'btns')
+    const editeBtnContents = elementMaker('div', 'd-flex justify-content-center')
+    const editBtn = elementMaker('a', 'icon-btn editBtn')
+    const editBtnIcon =  elementMaker('span', 'material-icons')
+    const deleteBtn = elementMaker('a', 'icon-btn removeBtn')
+    const deleteBtnIcon = elementMaker('span', 'material-icons')
+
+    
+
+    
+    editBtnIcon.appendChild(document.createTextNode('edit'))
+    editBtn.appendChild(editBtnIcon)
+    editeBtnContents.appendChild(editBtn)
+    editeBtnElement.appendChild(editeBtnContents)
+
+    
+    deleteBtnIcon.appendChild(document.createTextNode('delete_outline'))
+    deleteBtn.appendChild(deleteBtnIcon)
+    editeBtnContents.appendChild(deleteBtn)
+
+    
+    notesDatesTextElement.appendChild(document.createTextNode(`Last Update: ${dates()}`))
+    notesDatesElement.appendChild(notesDatesTextElement)
     noteBodyTextElement.appendChild(document.createTextNode(noteBodyText.value))
     noteHeadTextElemnt.appendChild(document.createTextNode(noteTitle.value))
     noteContinerElement.appendChild(noteHeadElement)
@@ -105,6 +172,8 @@ function addContent(e) {
     rootNoteElement.appendChild(seconderyNoteElement)
     noteArea.appendChild(rootNoteElement)
 
+    
+    storeDataFromLocalStoreage(noteTitle.value, noteBodyText.value)
     noteBodyText.value = ''
     noteTitle.value = ''
    
@@ -113,21 +182,58 @@ function addContent(e) {
     e.preventDefault()
 }
 
+// 
+function storeDataFromLocalStoreage(title, bodyText) {
+    let data;
+    if(localStorage.getItem('data') === null) {
+        data = []
+    }else{
+        data = JSON.parse(localStorage.getItem('data'))
+    }
+    data.push([title, bodyText])
+    localStorage.setItem('data', JSON.stringify(data))
+}
 
-//delete button event
+
+//delete card event
 function deleteAction(e) {
     let pareantElement = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
-    
-    if(e.target.parentNode.classList.contains('removeBtn')) {
+    console.log(pareantElement)
+    if(e.target.parentNode.classList.contains('removeBtn') && confirm('Are You Sure?')) {
+        
         pareantElement.remove('modal-root')
+        
+        //remove from LS
+        removeCardFromLocalStoreage(pareantElement)
+    }
+}
+
+//remove from LS
+function removeCardFromLocalStoreage(cardItem) {
+    //carrent element textContent selected 
+    let cartItemsTite = cardItem.firstChild.firstChild.firstChild.firstChild.textContent
+    
+    let data;
+    if(localStorage.getItem('data') === null) {
+        data = []
+    }else{
+        data = JSON.parse(localStorage.getItem('data'))
     }
     
+    data.forEach(function(item, index) {
+        
+        if(cartItemsTite === item[0]) {
+            data.splice(index, 1)
+        }
+    })
+    localStorage.setItem('data', JSON.stringify(data))
     
 }
 
 //edit content action
 function editAction(e) {
     let parentElement, headerText, bodyText, headTextElemnt, bodyTextElemnt, editButton;
+    
 
     if(e.target.parentNode.classList.contains('editBtn')) {
             parentElement = e.target.parentNode.parentNode.parentNode.parentNode
@@ -148,21 +254,29 @@ function editAction(e) {
         const textareaItem = elementMaker('textarea', 'form-control modal-head-text')
         textareaItem.setAttribute('gramm_editor', 'false')
         textareaItem.value = bodyText
+
         
         editButton.parentNode.replaceChild(reWriteItem, editButton)
         headTextElemnt.parentNode.replaceChild(inputItem ,headTextElemnt)
         bodyTextElemnt.parentNode.replaceChild(textareaItem , bodyTextElemnt)
         
+        
+        
     }
 }
 
 function reWriteAction(e) {
-    let parentElement, headerText, bodyText, headTextElemnt, bodyTextElemnt, reWriteBtn;
-
+    
+    let data;
+    if(localStorage.getItem('data') === null) {
+        data = []
+    }else{
+        data = JSON.parse(localStorage.getItem('data'))
+    }
+    let parentElement, headerText, bodyText, reWriteBtn;
     if(e.target.parentNode.classList.contains('rewrite')) {
         
         parentElement = e.target.parentNode.parentNode.parentNode.parentNode
-        console.log(parentElement)
         headerText = parentElement.firstChild.firstChild
         bodyText = parentElement.childNodes[2].firstChild
         reWriteBtn = e.target.parentNode.parentNode.parentNode.parentNode.lastChild.firstChild.firstChild
@@ -174,13 +288,27 @@ function reWriteAction(e) {
 
         const headerTextElement = elementMaker('h5', '')
         headerTextElement.appendChild(document.createTextNode(headerText.value))
-        const bodyTextElement = elementMaker('p', 'lead fix-size')
+        const bodyTextElement = elementMaker('p', 'fix-size')
         bodyTextElement.setAttribute('gramm_editor', 'false')
         bodyTextElement.appendChild(document.createTextNode(bodyText.value))
 
+        
+        
         headerText.parentNode.replaceChild(headerTextElement, headerText)
         bodyText.parentNode.replaceChild(bodyTextElement, bodyText)
         reWriteBtn.parentNode.replaceChild(editBtn, reWriteBtn)
+        
+
+        //selected element notes
+        let [skip, ...noteAreasElement] = noteArea.children
+        
+        data.forEach(function(item, index) {
+            if(noteAreasElement[index] === parentElement.parentNode.parentNode){
+                data[index] = [headerText.value, bodyText.value]
+            }
+               
+        })
+        localStorage.setItem('data', JSON.stringify(data))
         
     }
     
@@ -197,13 +325,9 @@ function reWriteAction(e) {
 // }
 
 
-//this function makes element with class return the element
-function elementMaker(tagName, nameOfClass) {
-    const element = document.createElement(tagName)
-    element.className = nameOfClass
 
-    return element
-}
+
+
 
 // //color plate event function 
 // function colorSelectore(e) {
@@ -224,22 +348,26 @@ function elementMaker(tagName, nameOfClass) {
 // }
 
 
+
+
+/**
+ * @reuseable function 
+**/
+
 //dates function 
 function dates() {
-    let monthName = ['January', 
-    'February',
-    'March',
-    'April',
-    'May', 
-    'June', 
-    'July', 
-    'August', 
-    'September',
-    'October',
-    'November',
-    'December']
+   
     let datemakers = new Date()
-    dateSetter.innerHTML = `Today : ${datemakers.getDate()} ${monthName[datemakers.getMonth()]} ${datemakers.getFullYear()}`
+    var todyDates = datemakers.toUTCString().match(/\d{2}.*\d{4}/g).join('')
+    dateSetter.innerHTML = `Today : ${todyDates}`
 
-    return `${datemakers.getDate()} ${monthName[datemakers.getMonth()]} ${datemakers.getFullYear()}`
+    return todyDates
+}
+
+//this function makes element with class return the element
+function elementMaker(tagName, nameOfClass) {
+    const element = document.createElement(tagName)
+    element.className = nameOfClass
+
+    return element
 }
